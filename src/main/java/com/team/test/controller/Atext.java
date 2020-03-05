1a48e549b9b40c41b00a9ed508d855c5afa27db7
hwy <624385630@qq.com>
2020-03-05 15:50:13 +0800

    我新增的文件

---
+++ src/main/java/com/team/test/controller/Atext.java
@@ -0,0 +1,14 @@
+package com.team.test.controller;
+
+/**
+ * @Author: 韩汪洋
+ * @BelongsProject: test
+ * @BelongsPackage: com.team.test.controller
+ * @CreateTime: 2020年03月05日15:42
+ * @Description: ${Description}
+ */
+public class Atext {
+    public static void main(String[] args) {
+        System.out.println("下载完之后新建的,准备更新到github");
+    }
+}
